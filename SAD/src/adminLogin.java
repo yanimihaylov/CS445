c10abcda8186064dbe463a2e0597470f45499f6a
Yani Mihaylov <ymihaylo@hawk.iit.edu>
2015-11-23 19:57:26 -0600

    Project

--- SAD/src/adminLogin.java
+++ SAD/src/adminLogin.java
@@ -31,15 +31,27 @@ public class adminLogin extends Login{
 	}
 	
 	public void createListing(){
-		Regular l1 = new Regular("Company 1","03/21/1994","04/21/1994","regular listing test");
-		HomePageFeatured l2 = new HomePageFeatured("Company 2","03/21/1994","04/21/1994","Home Page Featured listing test");
-		CategoryFeatured l3 = new CategoryFeatured("Company 3","03/21/1994","04/21/1994","Category Featured listing test");
-		
 		Listing[] listingArray = Login.getArray();
+		System.out.println("Select listing type");
+		String type = scan.next();
+		if(type.equalsIgnoreCase("Regular")){
+			
+			Regular l1 = new Regular("Company 1","03/21/1994","04/21/1994","regular listing test");
+			listingArray[0]=l1;
+		}
 		
-		listingArray[0]=l1;
-		listingArray[1]=l2;
-		listingArray[2]=l3;
+		if(type.equalsIgnoreCase("hpf")){
+			
+			HomePageFeatured l2 = new HomePageFeatured("Company 2","03/21/1994","04/21/1994","Home Page Featured listing test");
+			listingArray[1]=l2;
+		}
+		
+		if(type.equalsIgnoreCase("catfeatured")){
+			
+			CategoryFeatured l3 = new CategoryFeatured("Company 3","03/21/1994","04/21/1994","Category Featured listing test");
+			listingArray[2]=l3;
+		}
+		
 		options();
 	}
 	
