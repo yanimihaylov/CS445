cd5cb8855c2051d7b542d8e1709315b5c1a0e21b
Yani Mihaylov <ymihaylo@hawk.iit.edu>
2015-11-26 03:31:42 -0600

    deleted useless stuff

--- SAD/Test.java
+++ SAD/Test.java
@@ -5,24 +5,5 @@ public class Test {
 		Listing[] listingArray = new Listing[3];
 		Login l = new Login();
 		l.login();
-			
-		
-		/*Regular l1 = new Regular("Company 1","03/21/1994","04/21/1994","regular listing test");
-		HomePageFeatured l2 = new HomePageFeatured("Company 2","03/21/1994","04/21/1994","Home Page Featured listing test");
-		CategoryFeatured l3 = new CategoryFeatured("Company 3","03/21/1994","04/21/1994","Category Featured listing test");
-
-
-		System.out.println(l1.toString()+"\n");
-		System.out.println(l2.toString()+"\n");
-		System.out.println(l3.toString()+"\n"); 
-		
-		Listing[] listingArray = new Listing[3];
-		listingArray[0]=l1;
-		listingArray[1]=l2;
-		listingArray[2]=l3;
-		
-		for(int i=0; i<3; i++){
-			System.out.println("\n"+listingArray[i].toString());
-		} */
 	}
 }
